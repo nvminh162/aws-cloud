e58511037b7d3852f61261671cb88df48726db69
nvminh162 <nvminh162@gmail.com>
2025-12-28 15:54:44 +0700

    update welcome message in lab1-basic-ec2.sh for personalization and clarity

--- 07_computing-services-ec2/lab1-basic-ec2.sh
+++ 07_computing-services-ec2/lab1-basic-ec2.sh
@@ -5,11 +5,13 @@ sudo yum update -y
 #Instal HTTPD
 sudo yum install httpd -y
 
-# Edit file index: Modify below file:
+# Edit file index: Modify below file: ```sudo vi /var/www/html/index.html```
 /var/www/html/index.html
+
 #Add some html code:
-<h1>Welcome to Udemy</h1>
-<h2>AWS Cloud for beginner. Please like, subscribe and share !!!!</h2>
+<h1>Welcome to nvminh162</h1>
+<h2>My journey of learning, accumulating knowledge, and becoming a "Cloud Solution Architect (SA)"</h2>
+
 #Enable httpd
 sudo systemctl enable httpd
 sudo service httpd start
